8c192a00d31eec06eb973b264e906a831acc8c5b
decagon <udemekendrick@gmail.com>
2025-03-05 14:58:10 +0100

    max profit method added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,13 @@
 package org.example;
 
+import org.example.solutions.MaxProfit;
 import org.example.solutions.RemoveDuplicates;
-
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
-        RemoveDuplicates rd = new RemoveDuplicates();
-        int result = rd.removeDuplicate(nums);
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        MaxProfit mp = new MaxProfit();
+        int result = mp.maxProfit(prices);
         System.out.println(result);
     }
 }
