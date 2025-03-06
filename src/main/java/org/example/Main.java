3db11b8f336f58e3c339004b33688b5098119d8b
decagon <udemekendrick@gmail.com>
2025-03-06 22:12:44 +0100

    rotate array method added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import org.example.solutions.MaxProfit;
-import org.example.solutions.RemoveDuplicates;
+import org.example.solutions.RotateArray;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4};
-        MaxProfit mp = new MaxProfit();
-        int result = mp.maxProfit(prices);
-        System.out.println(result);
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        RotateArray rotateArray = new RotateArray();
+        int[] result = rotateArray.rotate(nums, k);
+        System.out.println(Arrays.toString(result));
     }
 }
